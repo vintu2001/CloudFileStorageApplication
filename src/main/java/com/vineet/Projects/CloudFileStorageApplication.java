104c44ac277bdb6d3cfc228c7f7d3257757cce9d
Vineet <vineet@Vineets-MacBook-Air.local>
2025-01-01 23:51:04 +0530

    Initial Project Push

---
+++ src/main/java/com/vineet/Projects/CloudFileStorageApplication.java
@@ -0,0 +1,14 @@
+package com.vineet.Projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileStorageApplication.class, args);
+		System.out.println("Cloud File Storage Application Now Running....");
+	}
+
+}
